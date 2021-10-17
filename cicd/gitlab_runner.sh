d3181a62f7c0988ab10ba4ef62b74061d8dab4de
Gleb Buzin <qufiwefefwoyn@gmail.com>
2021-10-17 22:00:22 +0300

    fix: modify gitlab runner sh

--- cicd/gitlab_runner.sh
+++ cicd/gitlab_runner.sh
@@ -8,7 +8,6 @@ sudo groupadd docker
 sudo usermod -aG docker "${USER}"
 newgrp docker
 
-
 docker volume create gitlab-runner-config
 
 docker run -d --name gitlab-runner --restart always \
@@ -16,12 +15,41 @@ docker run -d --name gitlab-runner --restart always \
     -v gitlab-runner-config:/etc/gitlab-runner \
     gitlab/gitlab-runner:latest
 
-docker run --rm -it -v gitlab-runner-config:/etc/gitlab-runner gitlab/gitlab-runner:latest register
 
-sed -i "s/changemename/runner${RANDOM}/g" /var/lib/docker/volumes/gitlab-runner-config/_data/config.toml
+read -d '' runnerconfig << EOF || true
+concurrent = 1
+check_interval = 0
+
+[session_server]
+  session_timeout = 900
+
+[[runners]]
+  name = "changemename"
+  url = "https://gitlab.com/"
+  token = "changemetoken"
+  executor = "docker"
+  [runners.custom_build_dir]
+  [runners.cache]
+    [runners.cache.s3]
+    [runners.cache.gcs]
+    [runners.cache.azure]
+  [runners.docker]
+    tls_verify = false
+    image = "docker:stable"
+    privileged = true
+    disable_entrypoint_overwrite = false
+    oom_kill_disable = false
+    disable_cache = false
+    volumes = ["/certs/client", "/cache"]
+    shm_size = 0
+EOF
+
+sudo echo "${runnerconfig}" > /var/lib/docker/volumes/gitlab-runner-config/_data/config.toml
+
+sudo sed -i "s/changemename/runner${RANDOM}/g" /var/lib/docker/volumes/gitlab-runner-config/_data/config.toml
 
 echo "Enter token: "
 read token
-sed -i "s/changemetoken/${token}/g" /var/lib/docker/volumes/gitlab-runner-config/_data/config.toml
+sudo sed -i "s/changemetoken/${token}/g" /var/lib/docker/volumes/gitlab-runner-config/_data/config.toml
 
 docker restart gitlab-runner
