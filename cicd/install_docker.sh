f3ddd872478a312c6c7153de47c616aaaa614b13
Gleb Buzin <qufiwefefwoyn@gmail.com>
2021-10-17 22:18:19 +0300

    fix: boom

--- cicd/install_docker.sh
+++ cicd/install_docker.sh
@@ -6,6 +6,8 @@ set -e
 
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
-sudo groupadd docker
+sudo groupadd docker || true
 sudo usermod -aG docker "${USER}"
 newgrp docker
+
+echo "Docker Installed!"
