31b0f877c94ede3059f381e6cb19cb876bee0164
Gleb Buzin <qufiwefefwoyn@gmail.com>
2021-10-17 22:14:18 +0300

    fix: another

--- cicd/gitlab_runner.sh
+++ cicd/gitlab_runner.sh
@@ -4,10 +4,22 @@
 
 # set -e
 
-curl -fsSL https://get.docker.com -o get-docker.sh
-sudo sh get-docker.sh
+sudo apt-get -y update
+sudo apt-get -y install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get -y update
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io
+
 sudo groupadd docker
-sudo usermod -aG docker "${USER}"
+sudo usermod -aG docker $USER
 newgrp docker
 
 docker volume create gitlab-runner-config
