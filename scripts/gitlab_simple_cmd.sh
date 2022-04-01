0ae975ccd82dd5a388afe8b86b4ab1325ce220fb
Gleb Buzin <qufiwefefwoyn@gmail.com>
2022-04-01 23:02:20 +0300

    fix: update gitlab scripts

--- scripts/gitlab_simple_cmd.sh
+++ scripts/gitlab_simple_cmd.sh
@@ -5,9 +5,8 @@ set -e
 
 export ACCESS_KEY=${ACCESS_KEY?Variable not set}
 export IP_ADDRESS=${IP_ADDRESS?Variable not set}
-export LOCAL_DIR=${LOCAL_DIR?Variable not set}
-export REMOTE_DIR_FULL=${REMOTE_DIR?Variable not set}
+export CMD_TO_EXEC=${CMD_TO_EXEC?Variable not set}
 
 eval "$(ssh-agent -s)"
 echo "$ACCESS_KEY" | tr -d '\r' | ssh-add -
-scp -o "StrictHostKeyChecking=no" -r "$LOCAL_DIR" "ubuntu@$IP_ADDRESS:$REMOTE_DIR_FULL"
+ssh -o "StrictHostKeyChecking=no" "ubuntu@$IP_ADDRESS" "${CMD_TO_EXEC}"
