d9cb294944219783ae04a14d070398a3b7f07f3e
Gleb Buzin <qufiwefefwoyn@gmail.com>
2022-04-01 23:27:43 +0300

    fix: fix gitlab simple transfer

--- scripts/gitlab_simple_transfer.sh
+++ scripts/gitlab_simple_transfer.sh
@@ -6,7 +6,7 @@ set -e
 export ACCESS_KEY=${ACCESS_KEY?Variable not set}
 export IP_ADDRESS=${IP_ADDRESS?Variable not set}
 export LOCAL_DIR=${LOCAL_DIR?Variable not set}
-export REMOTE_DIR_FULL=${REMOTE_DIR?Variable not set}
+export REMOTE_DIR_FULL=${REMOTE_DIR_FULL?Variable not set}
 
 eval "$(ssh-agent -s)"
 echo "$ACCESS_KEY" | tr -d '\r' | ssh-add -
